436be86596d5eeed20e58ba60099cfe114f80dbc
Jennifer <jleannk23@gmail.com>
2018-05-02 14:19:32 -0600

    navigation added

--- app/src/main/java/com/jenk/iparkittest_android/MainActivity.kt
+++ app/src/main/java/com/jenk/iparkittest_android/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.jenk.iparkittest_android
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -18,10 +17,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -58,22 +54,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_reservation -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
+            R.id.nav_signin -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_register -> {
 
             }
-            R.id.nav_manage -> {
+            R.id.nav_reciept -> {
 
             }
-            R.id.nav_share -> {
+            R.id.nav_rateApp -> {
 
             }
-            R.id.nav_send -> {
+            R.id.nav_contactUs -> {
 
             }
         }
